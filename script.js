01d11c8a4beed716d9b72aaebb8445b47c65420c
Ken Yamada <yamakenth@gmail.com>
2021-12-09 17:23:28 +0700

    Add function to display game stats message

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // play game 
 const playGame = (() => {  
-  // turn
-  let currPlayer = player1;
+  // initialize first player and print first message 
+  let currPlayer = firstPlayer;
+  displayController.printMessage(currPlayer.name);
   // eventListener on each square 
   const board = document.querySelectorAll('.square');
   board.forEach((square) => {
@@ -9,7 +10,7 @@ const playGame = (() => {
       gameBoard.update(e.target.id, currPlayer.symbol);
       displayController.render();
       (currPlayer === player1) ? currPlayer = player2 : currPlayer = player1;
-
+      displayController.printMessage(currPlayer.name);
     });
   });
 
@@ -51,10 +52,20 @@ const displayController = (() => {
       }
     }
   };
-  return { render };
+  // render message 
+  const printMessage = (playerName) => {
+    const message = document.querySelector('.message > h3');
+    message.textContent = `${playerName}'s turn`;
+  }
+  
+  return { 
+    render,
+    printMessage,
+  };
 })();
 
 // play
 const player1 = player('Ken', 'O');
 const player2 = player('Gem', 'X');
+let firstPlayer = player1;
 playGame();
