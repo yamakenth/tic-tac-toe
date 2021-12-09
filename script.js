3e8a313ef4cd26725ea152f74ebf76b97fea826d
Ken Yamada <yamakenth@gmail.com>
2021-12-09 17:14:52 +0700

    Add ability to update game board via click input

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
+// play game 
+const playGame = (() => {  
+  // turn
+  let currPlayer = player1;
+  // eventListener on each square 
+  const board = document.querySelectorAll('.square');
+  board.forEach((square) => {
+    square.addEventListener('click', (e) => {
+      gameBoard.update(e.target.id, currPlayer.symbol);
+      displayController.render();
+      (currPlayer === player1) ? currPlayer = player2 : currPlayer = player1;
+
+    });
+  });
+
+});
+
 // gameBoard module 
 const gameBoard = (() => {
+  // game board as array 
   let board = [
-    ['X', ' ', ' '],
-    ['O', 'O', 'O'],
-    [' ', ' ', 'X']
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
   ];
-
-  return { board };
+  // function to update board 
+  const update= (squareId, turn) => {
+    const row = squareId.split('-')[0][1];
+    const col = squareId.split('-')[1][1];
+    board[row][col] = turn;
+  };
+  return { 
+    board,
+    update,
+  };
 })();
 
 // player factory 
-const Player = () => {
-
+const player = (name, symbol) => {
+  return { name, symbol };
 }
 
 // display controller module 
 const displayController = (() => {
+  // render the whole board
   const render = () => {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
@@ -24,11 +51,10 @@ const displayController = (() => {
       }
     }
   };
-  
-  
-  
-  
   return { render };
 })();
 
-displayController.render();
+// play
+const player1 = player('Ken', 'O');
+const player2 = player('Gem', 'X');
+playGame();
