3a174b78c9b012aa248f282534fa0a2d32316d4d
Ken Yamada <yamakenth@gmail.com>
2021-12-09 21:41:49 +0700

    Change game logic

--- script.js
+++ script.js
@@ -1,142 +1,133 @@
-// play game 
-const playGame = (() => {  
-  // initialize first player and print first message 
-  let currPlayer = firstPlayer;
-  displayController.printMessage(currPlayer.name);
-
-  // eventListener on each square 
-  const board = document.querySelectorAll('.square');
-  board.forEach((square) => {
-    square.addEventListener('click', (e) => {
-      const updated = gameBoard.update(e.target.id, currPlayer.symbol);
-      if (updated) {
-        displayController.render();
-        if (checkWin()) {
-          console.log('there is a winner');
-          displayController.printWinMessage(currPlayer.name);
-          return;
-        } else if (checkTie()) {
-          console.log('it\s a tie');
-          displayController.printTieMessage();
-          return;
-        }
-        (currPlayer === player1) ? currPlayer = player2 : currPlayer = player1;
-        displayController.printMessage(currPlayer.name);
-      }
-    });
-  });
-
-  // check for win 
-  const checkWin = () => {
-    console.log('checking for win...');
-    const currBoard = gameBoard.board;
-    // check for horizontal win 
+// gameBoard module 
+// take in no parameters 
+// return advanceTurn function which return game status 
+// { win: 2, tie: 1, continue: 0 }
+const gameBoard = (() => {
+  // store board as 2d array 
+  let board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+  ]; 
+  // function to advance one turn and return game status 
+  function advanceTurn(squareId, playerMark) {
+    const row = squareId.split('-')[0][1];
+    const col = squareId.split('-')[1][1];
+    if (board[row][col] === ' ') {
+      board[row][col] = playerMark;
+      renderBoard();
+      console.log('The game Board was successfully updated and displayed.');
+      const gameStatus = generateGameStatus();
+      console.log({ gameStatus });
+      return gameStatus;
+    } else {
+      console.log('This square is disabled.');
+    }
+  };
+  // function to render game board on page
+  function renderBoard() {
     for (let i = 0; i < 3; i++) {
-      if (currBoard[i][0] !== ' ' && currBoard[i][0] === currBoard[i][1] && currBoard[i][1] === currBoard[i][2]) {
+      for (let j = 0; j < 3; j++) {
+        const square = document.querySelector(`#r${i}-c${j}`);
+        square.textContent = board[i][j]; 
+      }
+    }
+  };
+  // function to generate game status { win: 2, tie: 1, continue: 0 }
+  function generateGameStatus() {
+    if (isWin()) {
+      return 2;
+    } else if (isTie()) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+  // function to find if there is a winner 
+  function isWin() {
+    // check for horizontal win
+    for (let i = 0; i < 3; i++) {
+      if (board[i][0] !== ' ' && board[i].every(val => val === board[i][0])) {
         return true;
       }
     }
     // check for vertical win 
     for (let j = 0; j < 3; j++) {
-      if (currBoard[0][j] !== ' ' && currBoard[0][j] === currBoard[1][j] && currBoard[1][j] === currBoard[2][j]) {
+      if (board[0][j] !== ' ' && (board[0][j] === board[1][j] && board[1][j] === board[2][j])) {
         return true;
       }
     }
     // check for diagonal win 
     if (
-      (currBoard[0][0] !== ' ' && currBoard[0][0] === currBoard[1][1] && currBoard[1][1] === currBoard[2][2])
-      || (currBoard[0][2] !== ' ' && currBoard[0][2] === currBoard[1][1] && currBoard[1][1] === currBoard[2][0])
+      (board[0][0] !== ' ' && board[0][0] === board[1][1] && board[1][1] === board[2][2])
+      || (board[0][2] !== ' ' && board[0][2] === board[1][1] && board[1][1] === board[2][0])
     ) {
       return true;
     }
   };
-
-  // check for a tie 
-  const checkTie = () => {
+  // function to find if there is a tie
+  function isTie() {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        if (gameBoard.board[i][j] === ' ') {
+        if (board[i][j] === ' ') {
           return false;
         }
       }
     }
     return true;
   };
-});
 
-// gameBoard module 
-const gameBoard = (() => {
-  // game board as array 
-  let board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-  ];
-
-  // function to update board 
-  const update = (squareId, turn) => {
-    const row = squareId.split('-')[0][1];
-    const col = squareId.split('-')[1][1];
-    if (board[row][col] === ' ') {
-      board[row][col] = turn;
-      return true;
-    } else {
-      console.log('this square if disabled');
-      return false;
-    }
-  };
-
-  return { 
-    board,
-    update,
-  };
+  return {
+    advanceTurn,
+  }
 })();
+
 
 // player factory 
-const player = (name, symbol) => {
-  return { name, symbol };
-}
+// take in player name and mark = 'X' || 'O'
+// return object of a player 
+const player = (name, mark) => {
+  return { name, mark };
+} 
 
-// display controller module 
-const displayController = (() => {
-  // render the whole board
-  const render = () => {
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        const square = document.querySelector(`#r${i}-c${j}`);
-        square.textContent = gameBoard.board[i][j];
-      }
+
+// play the whole game 
+// take in no parameters 
+// return no results 
+const playGame = (() => {
+  // create players 
+  const player1 = player('Player 1', 'O');
+  const player2 = player('Player 2', 'X');
+  // initialize first player and display message 
+  let currPlayer = player1;
+  renderMessage(0);
+  // addEventListner to play one round after each click in a square
+  const board = document.querySelectorAll('.square');
+  board.forEach((square) => {
+    square.addEventListener('click', playOneTurn);
+  });
+  // function to play one turn after a click
+  function playOneTurn() {
+    console.log(`clicked square ${this.id}.`);
+    const gameStatus = gameBoard.advanceTurn(this.id, currPlayer.mark);
+    if (gameStatus === 0) { // continue
+      currPlayer = (currPlayer === player1) ? player2 : player1;
+    } else if (gameStatus === 1 || gameStatus === 2) { // tie or win 
+      board.forEach((square) => {
+        square.removeEventListener('click', playOneTurn);
+      });
     }
-  };
-
-  // render message 
-  const printMessage = (playerName) => {
-    const message = document.querySelector('.message > h3');
-    message.textContent = `${playerName}'s turn`;
-  };
-
-  // render win message 
-  const printWinMessage = (playerName) => {
-    const message = document.querySelector('.message > h3');
-    message.textContent = `${playerName} won!`;
-  };
-  
-  // render tie message 
-  const printTieMessage = () => {
-    const message = document.querySelector('.message > h3');
-    message.textContent = `It was a tie!`;
+    renderMessage(gameStatus);
   }
-  
-  return { 
-    render,
-    printMessage,
-    printWinMessage,
-    printTieMessage,
-  };
+  // function to diplay message
+  function renderMessage(gameStatus) {
+    const messageDisplay = document.querySelector('.message > h3');
+    if (gameStatus === 0) {
+      messageDisplay.textContent = `${currPlayer.name}'s turn`;
+    } else if (gameStatus == 1) {
+      messageDisplay.textContent = `It's a tie!`;
+    } else if (gameStatus === 2) {
+      messageDisplay.textContent = `${currPlayer.name} won!`;
+    }
+  } 
 })();
-
-// play
-const player1 = player('Player 1', 'O');
-const player2 = player('Player 2', 'X');
-let firstPlayer = player1;
-playGame();
