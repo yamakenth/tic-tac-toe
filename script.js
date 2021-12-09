ef45bdf44e2a0757c5ef2c3584e2fd2dc1d1bd31
Ken Yamada <yamakenth@gmail.com>
2021-12-09 16:48:47 +0700

    Add function to reder contents of game board

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 // gameBoard module 
 const gameBoard = (() => {
-  const board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
+  let board = [
+    ['X', ' ', ' '],
+    ['O', 'O', 'O'],
+    [' ', ' ', 'X']
   ];
 
-  return { 
-    board,
-  };
+  return { board };
 })();
 
 // player factory 
@@ -18,5 +16,19 @@ const Player = () => {
 
 // display controller module 
 const displayController = (() => {
-
+  const render = () => {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const square = document.querySelector(`#r${i}-c${j}`);
+        square.textContent = gameBoard.board[i][j];
+      }
+    }
+  };
+  
+  
+  
+  
+  return { render };
 })();
+
+displayController.render();
